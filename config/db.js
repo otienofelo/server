76ad84fc1f9eca4a0ddf3f2130d2ff314c9e2ddb
otienofelo <otienofelitichia@gmail.com>
2026-03-18 09:16:39 +0300

    fix: firebase env vars and db ssl for production

--- config/db.js
+++ config/db.js
@@ -1,19 +1,22 @@
-import pkg from "pg";
-import dotenv from "dotenv";
-
+import pg from 'pg';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
-pool.on("connect", () => {
-  console.log("Connected to the database");
+pool.on('connect', () => {
+  console.log('Database connected');
+});
+
+pool.on('error', (err) => {
+  console.error(' Database error:', err.message);
 });
 
 export default pool;
